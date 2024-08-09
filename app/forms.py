b8e61be8d7cecfb0b3fd4370cdc63e206513f6bc
Akhil Walia <akhilwalia318@gmail.com>
2024-08-08 20:50:20 -0400

    fixed register feedback

--- app/forms.py
+++ app/forms.py
@@ -1,23 +1,30 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
 from app.models import User
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(message="Username is required."), Length(min=3, max=20,
-                                              message="Username must be between 3 and 20 characters.")])
-    email = StringField('Email', validators=[DataRequired(message="Email is required."), Email(message="Invalid email address."), Length(max=254,
-                                           message="Email must be less than 254 characters.")])
-    password = PasswordField('Password', validators=[DataRequired(message="Password is required."), Length(min=6,
-                                                message="Password must be at least 6 characters.")])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(message="Password confirmation is required."),EqualTo('password',
-                                                         message="Passwords must match.")])
+    username = StringField('Username',
+                           validators=[DataRequired(message="Username is required."),
+                                       Length(min=3, max=20, message="Username must be between 3 and 20 characters.")])
+    email = StringField('Email',
+                        validators=[DataRequired(message="Email is required."),
+                                    Email(message="Invalid email address."),
+                                    Length(max=254, message="Email must be less than 254 characters.")])
+    password = PasswordField('Password',
+                             validators=[DataRequired(message="Password is required."),
+                                         Length(min=6, max=20, message="Password must be between 6 and 20 characters.")])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(message="Password confirmation is required."),
+                                                 EqualTo('password', message="Passwords must match.")])
     submit = SubmitField('Sign Up')
 
     def validate_email(self, email):
+        email.data = email.data.strip()
         user = User.get_by_email(email.data)
         if user:
             raise ValidationError('Email already registered. Please choose a different one.')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email(message="Invalid email address."), Length(max=254)])
